8c3c208b732fe13e5b98fd4f757d2e0bf1bdf495
LMilazzo <lucas.milazzo1311@gmail.com>
2025-10-14 23:56:05 -0400

    function fixes

--- App/Final/netlify/functions/detect-cards.js
+++ App/Final/netlify/functions/detect-cards.js
@@ -1,6 +1,7 @@
-const apiUrl = process.env.EC2_API_PUBLIC_IPv4;
-
 export async function handler(event, context) {
+
+  const apiUrl = Netlify.env.get("EC2_API_PUBLIC_IPv4");
+
   try {
     const response = await fetch(`${apiUrl}/detect-cards`, {
       method: 'POST',
